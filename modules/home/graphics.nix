2419e8dafd9138f5c81ed568972cf25c937cdaec
Geoff Davis <geoff@geoffdavis.com>
2026-05-29 11:44:09 -0700

    feat: move drawio allowlist to graphics module and remove from common config

--- modules/home/graphics.nix
+++ modules/home/graphics.nix
@@ -1,4 +1,15 @@
-{pkgs, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
+  # Central unfree allowlist for shared desktop packages.
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "drawio"
+      "obsidian"
+    ];
+
   # Graphics and diagramming tools
   home.packages = with pkgs; [
     drawio
